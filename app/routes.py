2322f1c039682eeb48f439897f03a91606f77df8
Sim4n6 <sim4n6@gmail.com>
2020-02-13 15:54:14 +0100

    Get of index now list all cmds
    - Get of index now list all cmds
    - improvements

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return get_cmd("index")
+    cmds_path = Path.cwd().joinpath("app","cmds")
+    cmds_available = [ x.stem for x in cmds_path.glob("*.txt") ]
+    return render_template("index.txt", cmds_available=cmds_available )
 
 
 @app.route('/<path:cmd>')
 def get_cmd(cmd):
     filename = cmd + ".txt"
-    path_file = Path.cwd().joinpath("app","cmds",filename)
-    if path_file.exists():
+    cmds_path = Path.cwd().joinpath("app","cmds")
+    filename_path = cmds_path.joinpath(filename)
+    if filename_path.exists():
         return render_template(filename)
     else :
         return "File not accessible"
