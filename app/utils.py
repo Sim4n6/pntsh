71e1c561b2759942d1d84329a4864d01fda3fbd7
Sim4n6 <sim4n6@gmail.com>
2020-02-14 15:16:50 +0100

    Using bash lexer rather than guessing lexer

--- app/utils.py
+++ app/utils.py
@@ -4,4 +4,4 @@ from pygments.lexers import BashLexer, guess_lexer
 from pygments.formatters import TerminalFormatter
 
 def colorize(text):
-    return highlight(text, guess_lexer(text), TerminalFormatter())
+    return highlight(text, BashLexer(), TerminalFormatter())
