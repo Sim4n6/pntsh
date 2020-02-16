e9c995a391faf79e057cfd55cd094da63bb18207
Sim4n6 <sim4n6@gmail.com>
2020-02-16 22:07:58 +0100

    Improvements for the highlighters of pntsh

--- app/utils.py
+++ app/utils.py
@@ -3,5 +3,21 @@ from pygments import highlight
 from pygments.lexers import BashLexer, guess_lexer
 from pygments.formatters import TerminalFormatter
 
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+class PntLexer(RegexLexer):
+    name = 'Pnt'
+    aliases = ['Pnt']
+
+    tokens = {
+        'root': [
+            (r'^#.*', Comment.Singleline),
+            (r'^\w+ ', Name.Builtin),
+            (r'-\w ', Operator),
+            (r'--\w ', Operator),
+        ]
+    }
+
 def colorize(text):
-    return highlight(text, BashLexer(), TerminalFormatter())
+    return highlight(text, PntLexer(), TerminalFormatter())
