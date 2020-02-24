981e65b7cb7c5c844b8f9fe1ea3f3750426a62d2
Sim4n6 <sim4n6@gmail.com>
2020-02-24 18:43:49 +0100

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -2,5 +2,5 @@ from app import app
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return "Page not found.\n", 404
+    return "Page not found. Please, feel free to contribute https://github.com/Sim4n6/pntsh .\n", 404
 
